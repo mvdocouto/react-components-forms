2868a67723eb16114b27d27c15441b77dc8ff54a
Marcus Couto <marcus.couto@m4u.com.br>
2018-03-05 19:26:42 -0300

    Inserção de keys para as opções do select.

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -3,9 +3,14 @@ import React from 'react'
 
 const Select = (props) => {
 
+   /*
+      Elementos multiplos necessitam de um id unico.
+      https://reactjs.org/docs/lists-and-keys.html#keys
+    */
+
   const choices = props.options.map(options => {
     return (
-      <option valeu={options.value}>{options.label}</option>
+      <option key={options.id} value={options.value}>{options.label}</option>
    );
 });
 
