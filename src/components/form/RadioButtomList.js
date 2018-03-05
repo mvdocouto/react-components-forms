c15465dae2d95ce60042d978a998388eab9f914d
Marcus Couto <marcus.couto@m4u.com.br>
2018-03-05 16:43:12 -0300

    Criação de um RadioButtomList

---
+++ src/components/form/RadioButtomList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import RadioButtom from './RadioButtom'
+
+const RadioButtomList = (props) => {
+
+    /*
+      Elementos multiplos necessitam de um id unico.
+      https://reactjs.org/docs/lists-and-keys.html#keys
+    */
+
+    const radioButtons = props.options.map(options => {
+        // console.log(props);
+        return (
+            
+            <RadioButtom
+            key={options.id}
+            name={props.name}
+            value={options.value}
+            label={options.label}
+            onChange={props.onChange}
+            checked={props.checkedOptions === options.value}
+            />
+
+         
+        );
+    });
+
+    return (
+        <div>
+            {radioButtons}
+        </div>
+    );
+}
+
+export default RadioButtomList;
